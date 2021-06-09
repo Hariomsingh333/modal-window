1d4687bc541c223986661d6de7bce0ae0b3c2ad7
Hari Om Singh <www.hariom03.com@gmail.com>
2021-06-09 14:19:34 +0530

    all js done

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnClose = document.querySelector(".close");
+const btnOpen = document.querySelectorAll(".button");
+
+btnOpen.forEach((text) => {
+  text.addEventListener("click", () => {
+    // console.log(`btn is clicked`);
+    modal.classList.remove("lock");
+    modal.classList.remove("overlay");
+  });
+});
+btnClose.addEventListener("click", () => {
+  modal.classList.add("lock");
+});
+overlay.addEventListener("click", () => {
+  modal.classList.add("lock");
+  overlay.classList.add("lock");
+});
